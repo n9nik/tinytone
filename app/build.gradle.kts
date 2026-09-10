0fa008f435006c09046c5bac75491df1299904c3
n9nik <nikhil.suresh@gmail.com>
2026-09-10 12:19:29 -0700

    Add missing compose foundation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,6 +113,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.6")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.exifinterface:exifinterface:1.4.1")
